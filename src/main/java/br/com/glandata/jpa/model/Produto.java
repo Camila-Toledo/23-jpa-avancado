b3fbee1bb7c549f9c8aa8500aeedab031c0f7f9b
Camila Toledo <toledo.camilla@hotmail.com>
2022-06-22 17:08:28 -0300

    Commit inicial do projeto.

---
+++ src/main/java/br/com/glandata/jpa/model/Produto.java
@@ -0,0 +1,67 @@
+package br.com.glandata.jpa.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NamedQuery(name = "Produto.produtosPorCategoria", query = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome")
+@Table(name = "produtos")
+@ToString
+public class Produto {
+
+	public Produto() {
+	}
+
+	public Produto(Long id) {
+		this.id = id;
+	}
+
+	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.categoria = categoria;
+	}
+
+	@Getter
+	@Setter
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Getter
+	@Setter
+	private String nome;
+
+	@Getter
+	@Setter
+	private String descricao;
+
+	@Getter
+	@Setter
+	private BigDecimal preco;
+
+	@Getter
+	@Setter
+	@ManyToOne
+	private Categoria categoria;
+
+	@Getter
+	@Setter
+	@Column(name = "data_cadastro")
+	private LocalDate dataCadastro = LocalDate.now();
+
+}
